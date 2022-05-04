bb78db2fe6063592cb55ffe0bfb0a4d10bd09494
Jean Boussier <jean.boussier@gmail.com>
2022-05-04 20:38:00 +0200

    Provide `RedisClient::Decorator` for easy compatibility clients

---
+++ test/redis_client/decorator_test.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class RedisClient
+  module DecoratorTests
+    include ClientTestHelper
+    include RedisClientTests
+
+    module Commands
+      def exists?(key)
+        call("EXISTS", key) { |c| c > 0 }
+      end
+    end
+
+    MyDecorator = Decorator.create(Commands)
+    class MyDecorator
+      def test?
+        true
+      end
+    end
+
+    def test_custom_command_helpers
+      @redis.call("SET", "key", "hello")
+      assert_equal 1, @redis.call("EXISTS", "key")
+      assert_equal true, @redis.exists?("key")
+      assert_equal([true], @redis.pipelined { |p| p.exists?("key") })
+      assert_equal([true], @redis.multi { |p| p.exists?("key") })
+    end
+
+    def test_client_methods_not_available_on_pipelines
+      assert_equal true, @redis.test?
+
+      @redis.pipelined do |pipeline|
+        assert_equal false, pipeline.respond_to?(:test?)
+      end
+
+      @redis.multi do |pipeline|
+        assert_equal false, pipeline.respond_to?(:test?)
+      end
+    end
+  end
+
+  class DecoratorTest < Minitest::Test
+    include DecoratorTests
+
+    private
+
+    def new_client(**overrides)
+      MyDecorator.new(RedisClient.config(**tcp_config.merge(overrides)).new_client)
+    end
+  end
+
+  class PooledDecoratorTest < Minitest::Test
+    include DecoratorTests
+
+    private
+
+    def new_client(**overrides)
+      MyDecorator.new(RedisClient.config(**tcp_config.merge(overrides)).new_pool)
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "redis-client"
+require "redis_client/decorator"
 require "toxiproxy"
 
 Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
