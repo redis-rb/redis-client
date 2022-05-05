4ad7ff7729215636490e83c924dfc39018bb8bb8
Jean Boussier <jean.boussier@gmail.com>
2022-05-05 11:45:44 +0200

    Call GC.verify_compaction_references before running the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,3 +25,10 @@ if ENV["DRIVER"] == "hiredis"
   require "hiredis"
   Redis.new(host: Servers::HOST, port: Servers::REDIS_TCP_PORT, driver: :hiredis).ping
 end
+
+begin
+  # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
+  # move objects around, helping to find object movement bugs.
+  GC.verify_compaction_references(double_heap: true, toward: :empty)
+rescue NoMethodError
+end
