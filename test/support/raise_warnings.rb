9e4e713f8afb14e7357f24fbc85d08db6b66ed48
Jean Boussier <jean.boussier@gmail.com>
2022-08-11 19:57:36 +0200

    Fix the signature of RaiseWarnings on older rubies

--- test/support/raise_warnings.rb
+++ test/support/raise_warnings.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 module RaiseWarnings
-  def warn(message, *, **)
+  def warn(message, *)
     super
     raise message
   end
+  ruby2_keywords :warn if respond_to?(:ruby2_keywords)
 end
 Warning.singleton_class.prepend(RaiseWarnings)
