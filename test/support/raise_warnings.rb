02cbd38e7db5a006fcaa332a1ef5fc1141d76579
Jean Boussier <jean.boussier@gmail.com>
2022-08-13 15:50:45 +0200

    Fix strict warnings module

--- test/support/raise_warnings.rb
+++ test/support/raise_warnings.rb
@@ -5,6 +5,6 @@ module RaiseWarnings
     super
     raise message
   end
-  ruby2_keywords :warn if respond_to?(:ruby2_keywords)
+  ruby2_keywords :warn if respond_to?(:ruby2_keywords, true)
 end
 Warning.singleton_class.prepend(RaiseWarnings)
